3719cd2f2531d00eed58bd29f2fe9dc647f7bf35
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-10-03 14:34:01 -0300

    Upload de Imagens e uso da Câmera no React Native

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,40 +1,67 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-import { StatusBar } from 'react-native';
+import { StatusBar, Modal, AsyncStorage } from 'react-native';
 
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
-import { 
+import { RNCamera } from 'react-native-camera';
+
+import api from '../../services/api';
+
+import {
   Container,
   AnnotationContainer,
-  AnnotationText 
+  AnnotationText,
+  NewButtonContainer,
+  ButtonsWrapper,
+  CancelButtonContainer,
+  SelectButtonContainer,
+  ButtonText,
+  Marker,
+  ModalContainer,
+  ModalImagesListContainer,
+  ModalImagesList,
+  ModalImageItem,
+  ModalButtons,
+  CameraButtonContainer,
+  CancelButtonText,
+  ContinueButtonText,
+  TakePictureButtonContainer,
+  TakePictureButtonLabel,
+  DataButtonsWrapper,
+  MarkerContainer,
+  MarkerLabel,
+  Form,
+  Input,
 } from './styles';
 
 export default class Main extends Component {
   static navigationOptions = {
     header: null,
-  }
+  };
 
   state = {
     locations: [],
+    newRealty: false,
+    cameraModalOpened: false,
+    dataModalOpened: false,
+    realtyData: {
+      location: {
+        latitude: null,
+        longitude: null,
+      },
+      name: 'Rocketseat',
+      //price: '10000',
+      address: 'Rua zero, 0',
+      images: [],
+    },
+  };
+
+  componentDidMount() {
+    this.getLocation();
   }
 
-  renderLocations = () => (
-    this.state.locations.map(location => (
-      <MapboxGL.PointAnnotation
-        id={location.id.toString()}
-        coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
-      >
-        <AnnotationContainer>
-          <AnnotationText>{location.price}</AnnotationText>
-        </AnnotationContainer>
-        <MapboxGL.Callout title={location.title}/>
-      </MapboxGL.PointAnnotation>
-    ))
-  )
-
-  async componentDidMount() {
+  getLocation = async () => {
     try {
       const response = await api.get('/ballads', {
         params: {
@@ -49,6 +76,265 @@ export default class Main extends Component {
     }
   }
 
+  handleNewRealtyPress = () => this.setState({ newRealty: !this.state.newRealty })
+
+  handleCameraModalClose = () => this.setState({ cameraModalOpened: !this.state.cameraModalOpened })
+
+  handleDataModalClose = () => this.setState({
+    dataModalOpened: !this.state.dataModalOpened,
+    cameraModalOpened: false,
+  })
+
+  handleGetPositionPress = async () => {
+    try {
+      const [longitude, latitude] = await this.map.getCenter();
+      this.setState({
+        cameraModalOpened: true,
+        realtyData: {
+          ...this.state.realtyData,
+          location: {
+            latitude,
+            longitude,
+          },
+        },
+      });
+    } catch (err) {
+      console.tron.log(err);
+    }
+  }
+
+  handleTakePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true, forceUpOrientation: true, fixOrientation: true, };
+      const data = await this.camera.takePictureAsync(options)
+      const { realtyData } = this.state;
+      this.setState({ realtyData: {
+        ...realtyData,
+        images: [
+          ...realtyData.images,
+          data,
+        ]
+      }})
+    }
+  }
+
+  handleNameChange = name => {
+    const { realtyData } = this.state;
+    this.setState({ realtyData: {
+      ...realtyData,
+      name,
+    }});
+  }
+
+  handleAddressChange = address => {
+    const { realtyData } = this.state;
+    this.setState({ realtyData: {
+      ...realtyData,
+      address,
+    }});
+  }
+
+  /*handlePriceChange = price => {
+    const { realtyData } = this.state;
+    this.setState({ realtyData: {
+      ...realtyData,
+      price,
+    }});
+  }*/
+
+  saveRealty = async () => {
+    try {
+      const {
+        realtyData: {
+          name,
+          address,
+          //price,
+          location: {
+            latitude,
+            longitude
+          },
+          images
+        }
+      } = this.state;
+
+      const newRealtyResponse = await api.post('/ballads', {
+        title: name,
+        address,
+        //price,
+        latitude: Number(latitude.toFixed(6)),
+        longitude: Number(longitude.toFixed(6)),
+      });
+
+      const imagesData = new FormData();
+
+      images.forEach((image, index) => {
+        imagesData.append('image', {
+          uri: image.uri,
+          type: 'image/jpeg',
+          name: `${newRealtyResponse.data.title}_${index}.jpg`
+        });
+      });
+
+      await api.post(
+        `/ballads/${newRealtyResponse.data.id}/images`,
+        imagesData,
+      );
+
+      this.getLocation()
+      this.handleDataModalClose()
+      this.setState({ newRealty: false });
+    } catch (err) {
+      console.tron.log(err);
+    }
+  }
+
+  renderConditionalsButtons = () => (
+    !this.state.newRealty ? (
+      <NewButtonContainer onPress={this.handleNewRealtyPress}>
+        <ButtonText>Nova Balada</ButtonText>
+      </NewButtonContainer>
+    ) : (
+      <ButtonsWrapper>
+        <SelectButtonContainer onPress={this.handleGetPositionPress}>
+          <ButtonText>Selecionar localização</ButtonText>
+        </SelectButtonContainer>
+        <CancelButtonContainer onPress={this.handleNewRealtyPress}>
+          <ButtonText>Cancelar</ButtonText>
+        </CancelButtonContainer>
+      </ButtonsWrapper>
+    )
+  )
+
+  renderLocations = () => (
+    this.state.locations.map(location => (
+      <MapboxGL.PointAnnotation
+        id={location.id.toString()}
+        coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
+      >
+        <AnnotationContainer>
+          
+        </AnnotationContainer>
+        <MapboxGL.Callout title={location.title} />
+      </MapboxGL.PointAnnotation>
+    ))
+  )
+
+  renderMarker = () => (
+    this.state.newRealty && !this.state.cameraModalOpened ? (
+      <Marker resizeMode="contain" source={require('../../images/marker.png')} />
+    ) : null
+  )
+
+  renderImagesList = () => (
+    this.state.realtyData.images.length !== 0 ? (
+      <ModalImagesListContainer>
+        <ModalImagesList horizontal>
+          { this.state.realtyData.images.map(image => (
+            <ModalImageItem source={{ uri: image.uri }} resizeMode="stretch" />
+          ))}
+        </ModalImagesList>
+      </ModalImagesListContainer>
+    ) : null
+  )
+
+  renderCameraModal = () => (
+    <Modal
+      visible={this.state.cameraModalOpened}
+      transparent={false}
+      animationType="slide"
+      onRequestClose={this.handleCameraModalClose}
+    >
+      <ModalContainer>
+        <ModalContainer>
+          <RNCamera
+            ref={camera => {
+              this.camera = camera;
+            }}
+            style={{ flex: 1 }}
+            type={RNCamera.Constants.Type.back}
+            autoFocus={RNCamera.Constants.AutoFocus.on}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            permissionDialogTitle={"Permission to use camera"}
+            permissionDialogMessage={
+              "We need your permission to use your camera phone"
+            }
+          />
+          <TakePictureButtonContainer onPress={this.handleTakePicture}>
+            <TakePictureButtonLabel />
+          </TakePictureButtonContainer>
+        </ModalContainer>
+        { this.renderImagesList() }
+        <ModalButtons>
+          <CameraButtonContainer onPress={this.handleCameraModalClose}>
+            <CancelButtonText>Cancelar</CancelButtonText>
+          </CameraButtonContainer>
+          <CameraButtonContainer onPress={this.handleDataModalClose}>
+            <ContinueButtonText>Continuar</ContinueButtonText>
+          </CameraButtonContainer>
+        </ModalButtons>
+      </ModalContainer>
+    </Modal>
+  )
+
+  renderDataModal = () => (
+    <Modal
+      visible={this.state.dataModalOpened}
+      transparent={false}
+      animationType="slide"
+      onRequestClose={this.handleDataModalClose}
+    >
+      <ModalContainer>
+        <ModalContainer>
+          <MapboxGL.MapView
+            centerCoordinate={[
+              this.state.realtyData.location.longitude,
+              this.state.realtyData.location.latitude
+            ]}
+            style={{ flex: 1 }}
+            styleURL={MapboxGL.StyleURL.Dark}
+          >
+            <MapboxGL.PointAnnotation
+              id="center"
+              coordinate={[
+                this.state.realtyData.location.longitude,
+                this.state.realtyData.location.latitude
+              ]}
+            >
+              <MarkerContainer>
+                <MarkerLabel />
+              </MarkerContainer>
+            </MapboxGL.PointAnnotation>
+          </MapboxGL.MapView>
+        </ModalContainer>
+        { this.renderImagesList() }
+        <Form>
+          <Input
+            placeholder="Nome da Balada"
+            value={this.state.realtyData.name}
+            onChangeText={this.handleNameChange}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <Input
+            placeholder="Endereço"
+            value={this.state.realtyData.address}
+            onChangeText={this.handleAddressChange}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </Form>
+        <DataButtonsWrapper>
+          <SelectButtonContainer onPress={this.saveRealty}>
+            <ButtonText>Salvar Balada</ButtonText>
+          </SelectButtonContainer>
+          <CancelButtonContainer onPress={this.handleDataModalClose}>
+            <ButtonText>Cancelar</ButtonText>
+          </CancelButtonContainer>
+        </DataButtonsWrapper>
+      </ModalContainer>
+    </Modal>
+  )
+
   render() {
     return (
       <Container>
@@ -57,16 +343,15 @@ export default class Main extends Component {
           centerCoordinate={[-49.6446024, -27.2108001]}
           style={{ flex: 1 }}
           styleURL={MapboxGL.StyleURL.Dark}
-          >
-             { this.renderLocations() }
+          ref={(map) => { this.map = map; }}
+        >
+          {this.renderLocations()}
         </MapboxGL.MapView>
+        { this.renderConditionalsButtons() }
+        { this.renderMarker() }
+        { this.renderCameraModal() }
+        { this.renderDataModal() }
       </Container>
     );
   }
 }
-
-Main.navigationOptions = {
-  header: null,
-};
-
-export default Main;
